1d5ec9ffc7b46a13e8832125b3ae8679714a8739
Arshdeep Singh <arshsherdill@gmail.com>
2017-08-19 18:52:50 +0530

    assignment 1 done testing to be done

--- Assignments/assignment1_updated/FabricBreakup.java
+++ Assignments/assignment1_updated/FabricBreakup.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class FabricBreakup
 {
 	public static int[] clothesArray;
@@ -20,78 +23,93 @@ public class FabricBreakup
 		favArray      = new int[n+1];
 		favShirtIndex = -1;
 		top = -1;
-		maxLiking = -1;
-	}
-
-	public static void main(String args[])
-	{
-		Scanner input = new Scanner(System.in);
-		int id;
-		int mode;
-		int temp;
-		int temp1;
-		int likingValue;
-		//System.out.println("Num of Operations");
-		int in = input.nextInt();
-		//System.out.println("Number of Operations is: " + in);
-		FabricBreakup fb = new FabricBreakup(in+1);
-
-		for(int i=0; i<in; i++)
-		{
-			id 	 = input.nextInt();
-			//System.out.println("id: " + id);
-			mode = input.nextInt();
-			//System.out.println("mode: "+ mode);
-			//mode 1 to put and mode 2 to party
-			if(mode==1)
-			{
-				//put shirt in stack
-				likingValue = input.nextInt();
-				//System.out.println("Liking Value is " + likingValue);
-				if(likingValue>=maxLiking)
-				{
-					//System.out.println("Greater than Max Liking");
-					favShirtIndex += 1;
-					top += 1;
-					clothesArray[top] = likingValue;
-					favArray[favShirtIndex] = top;
-					maxLiking = likingValue;
-				}
-				else
-				{	
-					//System.out.println("Lesser than Max Liking");
-					top += 1;
-					clothesArray[top] = likingValue;
-				}
-			}
-			else
-			{
-				if(top == -1)
-				{
-					System.out.println(id +" "+ -1);
-				}
-				else
-				{
-					//remove the favourite shirt and count how many shirts were kept back into the heap
-					temp1 = top;
-					top = favArray[favShirtIndex] - 1;
-					temp = favArray[favShirtIndex];
-					favShirtIndex -= 1;
-					if(favShirtIndex==-1)
-					{
-						maxLiking=-1;
-					}
-					temp = temp1-temp;
-					//System.out.println("Renoving Shirts, taking out the favourite one");
-					//System.out.println("Total of " + temp + " shirts removed.");
-					System.out.println(id + " " + temp);
-				}
-
-			}
-			if(i==in-1)
-			{
-				input.close();
-			}
+			maxLiking = -1;
 		}
-	}
+	
+		public static void main(String args[])
+		{
+			//if(args.length>0)
+			//
+			try
+			{
+				File file = new File(args[0]);
+				Scanner input = new Scanner(file);
+				//}
+			//else
+			//{
+			//	Scanner input = new Scanner(System.in);
+			//}
+			int id;
+			int mode;
+			int temp;
+			int temp1;
+			int likingValue;
+			//System.out.println("Num of Operations");
+			int in = input.nextInt();
+			//System.out.println("Number of Operations is: " + in);
+			FabricBreakup fb = new FabricBreakup(in+1);
+	
+			for(int i=0; i<in; i++)
+			{
+				id 	 = input.nextInt();
+				//System.out.println("id: " + id);
+				mode = input.nextInt();
+				//System.out.println("mode: "+ mode);
+				//mode 1 to put and mode 2 to party
+				if(mode==1)
+				{
+					//put shirt in stack
+					likingValue = input.nextInt();
+					//System.out.println("Liking Value is " + likingValue);
+					if(likingValue>=maxLiking)
+					{
+						//System.out.println("Greater than Max Liking");
+						favShirtIndex += 1;
+						top += 1;
+						clothesArray[top] = likingValue;
+						favArray[favShirtIndex] = top;
+						maxLiking = likingValue;
+					}
+					else
+					{	
+						//System.out.println("Lesser than Max Liking");
+						top += 1;
+						clothesArray[top] = likingValue;
+					}
+				}
+				else
+				{
+					if(top == -1)
+					{
+						System.out.println(id +" "+ -1);
+					}
+					else
+					{
+						//remove the favourite shirt and count how many shirts were kept back into the heap
+						temp1 = top;
+						top = favArray[favShirtIndex] - 1;
+						temp = favArray[favShirtIndex];
+						favShirtIndex -= 1;
+						if(favShirtIndex==-1)
+						{
+							maxLiking=-1;
+						}
+						temp = temp1-temp;
+						//System.out.println("Renoving Shirts, taking out the favourite one");
+						//System.out.println("Total of " + temp + " shirts removed.");
+						System.out.println(id + " " + temp);
+					}
+	
+				}
+				if(i==in-1)
+				{
+					input.close();
+				}
+			}
+			}
+			catch(FileNotFoundException e)
+			{
+				System.err.println(e.getMessage());
+			}			
+		}
 }
